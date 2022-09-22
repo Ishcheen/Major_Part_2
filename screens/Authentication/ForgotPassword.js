3ce391e87fc343e872104f43f2ee96ffdc940cb7
s-chandr <chandrasayan15@gmail.com>
2022-09-22 11:25:25 +0530

    part2complete

--- screens/Authentication/ForgotPassword.js
+++ screens/Authentication/ForgotPassword.js
@@ -13,6 +13,9 @@ import { utils } from  ".../../utils"
 const ForgotPassword = () => {
     const[email,setEmail] = React.useState("")
     const[emailError, setEmailError] = React.useState("")
+    function isEnableSendEmail() {
+        return email != "" && emailError == ""
+    }
     return (
         <AuthLayout
         title="Password Recovery"
@@ -26,7 +29,51 @@ const ForgotPassword = () => {
             style = {{ 
                 flex : 1 ,
                 marginTop:SIZES.padding * 2
-            }}></View>
+            }}>
+                <FormInput
+                    label="Email"
+                    keyboardType="email-address"
+                    autoCompleteType="email"
+                    onChange={(value) => {
+                        // validate email I
+                    utils.validateEmail(value, setEmailError)
+                    setEmail(value)
+                    }}
+                    
+                    errorMsg={emailError}
+                    appendComponent={
+                    <View
+                        style={{
+                            justifyContent: 'center'
+                        }}
+                        >
+                    <Image
+                        source={ email == ""  || ( email != ""  && emailError == "")  ? icons.correct : icons.cancel}
+                        style={{
+                            height: 20,
+                            width:20,
+                            tintColor: email == "" ? COLORS.gray : (email != "" && emailError == "") ? COLORS.green : COLORS.red
+                    }} />
+                    </View>
+                    }
+                    />
+
+            </View>
+
+            <TextButton
+            label = "send email"
+            disabled = {isEnableSendEmail() ? false :true }
+            buttonContainerStyle={{
+                height: 55,
+                alignItems: "center",
+                marginTop: SIZES.padding,
+                borderradius: SIZES.radius,
+                backgroundColor:isEnableSendEmail()?COLORS.primary:COLORS.transparentPrimary 
+            }}
+            onpress = {()=> navigation.goBack()}
+            >
+
+            </TextButton>
         </AuthLayout>
 
     )
